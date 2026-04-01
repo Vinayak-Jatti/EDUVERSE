8b8c700631091dd803e05a11022be4761d54f578
vinayak-jatti <vinayakjatti044@gmail.com>
2026-04-01 12:18:49 +0530

    feat: initialize express application loader with security, logging, and rate-limiting middleware

--- backend/src/loaders/express.loader.js
+++ backend/src/loaders/express.loader.js
@@ -37,7 +37,7 @@ export default (app) => {
       }
     }
   }));
-  app.use(cors({ origin: config.cors.origin, credentials: true }));
+  app.use(cors({ origin: config.cors.origin || config.cors.origin2 , credentials: true }));
   app.use(xssClean());
 
   // 📝 Request Tracing & Logging
